e98924b69a277b09e133a1ecb2a792490ec10574
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-11-04 21:43:04 -0700

    add finite_horizon_optimal_control

--- finite_horizon_optimal_control/main.py
+++ finite_horizon_optimal_control/main.py
@@ -14,10 +14,30 @@ def CalcFiniteHorizonOptimalInput(A,B,Q,R,P,N,x0):
     Calc Finite Horizon Optimal Input
 
     # TODO optimize
-        size check
-        search kron
+    in: see below 
+
+    min x'Px+sum(x'Qx+u'Ru)
+    s.t xk+1=Axk+Bu
+
+    out: uopt optimal input
     """
-    print("CalcFiniteHorizonOptimalInput start")
+    #  print("CalcFiniteHorizonOptimalInput start")
+
+    # data check
+    if A.shape[1] is not x0.shape[0]:
+        print("Data Error: A's col == x0's row")
+        print("A shape:")
+        print(A.shape)
+        print("x0 shape:")
+        print(x0.shape)
+        return None
+    elif B.shape[1] is not R.shape[1]:
+        print("Data Error: B's col == R's row")
+        print("B shape:")
+        print(B.shape)
+        print("R's shape:")
+        print(R.shape)
+        return None
 
     sx=np.eye(A.ndim)
     su=np.zeros((A.ndim,B.shape[1]*N))
@@ -53,13 +73,12 @@ def CalcFiniteHorizonOptimalInput(A,B,Q,R,P,N,x0):
     Qbar=np.kron(tm1,Q)+np.kron(tm2,P)
     Rbar=np.kron(np.eye(N),R)
 
-    uBa=-(su.T*Qbar*su+Rbar).I*su.T*Qbar*sx*x0
+    uopt=-(su.T*Qbar*su+Rbar).I*su.T*Qbar*sx*x0
     #  print(uBa)
     costBa=x0.T*(sx.T*Qbar*sx-sx.T*Qbar*su*(su.T*Qbar*su+Rbar).I*su.T*Qbar*sx)*x0
     #  print(costBa)
 
-    #  print(su)
-    #  print(sx)
+    return uopt
 
 
 if __name__ == '__main__':
@@ -82,8 +101,25 @@ if __name__ == '__main__':
     P=np.matrix([[1500,0.0],[0.0,100]])
     print("P")
     print(P)
-    N=3#Number of horizon
+    N=20#Number of horizon
 
-    ustar=CalcFiniteHorizonOptimalInput(A,B,Q,R,P,N,x0)
+    uopt=CalcFiniteHorizonOptimalInput(A,B,Q,R,P,N,x0)
 
+    #simulation
+    u_history=[]
+    x1_history=[]
+    x2_history=[]
+    x=x0
+    for u in uopt:
+        u_history.append(float(u[0]))
+        x=A*x+B*u
+        x1_history.append(float(x[0]))
+        x2_history.append(float(x[1]))
+
+    plt.plot(u_history,"-r",label="input")
+    plt.plot(x1_history,"-g",label="x1")
+    plt.plot(x2_history,"-b",label="x2")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
 
