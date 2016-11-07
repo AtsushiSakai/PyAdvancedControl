e1f8f4e560f2fea83ae5977b044a4eb3294428a5
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-11-06 23:04:31 -0800

    be on the safe side

--- mpc_path_tracking/main.py
+++ mpc_path_tracking/main.py
@@ -13,6 +13,11 @@ import matplotlib.pyplot as plt
 from math import * 
 import time
 
+dt=0.5  #[s] discrete time
+lr=1.0  #[m]
+T = 10  #number of horizon
+target=[-10,5]#[x,y]
+
 def LinealizeCarModel(xb,u,dt,lr):
     u"""
     TODO conplete model
@@ -25,25 +30,28 @@ def LinealizeCarModel(xb,u,dt,lr):
 
     a=u[0]
     beta=u[1]
+
+    t1=-dt*v*sin(theta+beta)
+    t2=dt*v*cos(theta+beta)
     
     A=np.eye(xb.shape[0])
     A[0,2]=dt*cos(theta+beta)
     A[1,2]=dt*sin(theta+beta)
     A[3,2]=dt*sin(beta)/lr
+    A[0,3]=t1
+    A[1,3]=t2
 
     B=np.zeros((xb.shape[0],u.shape[0]))
     B[2,0]=dt
+    B[0,1]=t1
+    B[1,1]=t2
     B[3,1]=dt*v*cos(beta)/lr
 
     return A,B
 
 def CalcInput(A,B,x,u):
 
-    T = 10  #number of horizon
-
-    ustar=[]
     x_0 =x
-
     x = Variable(x.shape[0], T+1)
     u = Variable(u.shape[0], T)
 
@@ -51,16 +59,17 @@ def CalcInput(A,B,x,u):
     states = []
     for t in range(T):
         cost = sum_squares(u[:,t])
-        #  cost = sum_squares(0)
-        if t == T-1:
-            #  cost+=sum_squares(x[1,t]-10.0)*1000
-            cost+=sum_squares(x[0,t]-10.0)*1000
-            cost+=sum_squares(x[1,t]-10.0)*1000
-        constr = [x[:,t+1] == A*x[:,t] + B*u[:,t]]
+        cost+=sum_squares(x[0,t]-target[0])*10.0
+        cost+=sum_squares(x[1,t]-target[1])*10.0
+        if t==T-1:
+            cost+=sum_squares(x[0,t]-target[0])*1000.0
+            cost+=sum_squares(x[1,t]-target[1])*1000.0
+ 
+        constr = [x[:,t+1] == A*x[:,t] + B*u[:,t], abs(u[:,t])<=0.1,abs(x[2,t])<= 5.0]
         states.append( Problem(Minimize(cost), constr) )
 
     prob = sum(states)
-    prob.constraints += [x[:,0] == x_0]
+    prob.constraints += [x[:,0] == x_0,x[2,T] == 0.0]
 
     start = time.time()
     #  result=prob.solve(verbose=True)
@@ -68,35 +77,51 @@ def CalcInput(A,B,x,u):
     elapsed_time = time.time() - start
     print ("calc time:{0}".format(elapsed_time)) + "[sec]"
 
-    ustar=u
+    #  print(prob.status)
+    return u,x
 
-    #  print(x.value)
-    print(u.value)
-
-    return ustar
+def GetListFromMatrix(x):
+    return np.array(x).flatten().tolist()
 
 
 def Main():
-
     x0=np.matrix([0.0,0.0,0.0,0.0]).T#[x,y,v theta]
     x=x0
     u=np.matrix([0.0,0.00]).T#[a,beta]
-    dt=0.1#[s]
-    lr=1.0#[m]
 
-    for i in range(1000):
+    for i in range(100):
         A,B=LinealizeCarModel(x,u,dt,lr)
-        ustar=CalcInput(A,B,x,u)
+        ustar,xstar=CalcInput(A,B,x,u)
         
-        u[0,0]=float(ustar[0,0].value)
+        u[0,0]=GetListFromMatrix(ustar.value[0,:])[0]
         u[1,0]=float(ustar[1,0].value)
 
         x=A*x+B*u
 
+        plt.subplot(2, 1, 1)
+        plt.plot(target[0],target[1],'xb')
         plt.plot(x[0],x[1],'.r')
         plt.axis("equal")
+        plt.xlabel("x[m]")
+        plt.ylabel("y[m]")
         plt.grid(True)
-        plt.pause(0.001)
+
+        plt.subplot(2, 1, 2)
+        plt.cla()
+        plt.plot(GetListFromMatrix(xstar.value[2,:]),'-b')
+        plt.ylim([-1.0,1.0])
+        plt.ylabel("velocity[m/s]")
+        plt.xlabel("horizon")
+
+        plt.grid(True)
+        plt.pause(0.0001)
+
+        dis=np.linalg.norm([x[0]-target[0],x[1]-target[1]])
+        if (dis<0.1):
+            print("Goal")
+            break
+
+    plt.show()
 
 
 if __name__ == '__main__':
