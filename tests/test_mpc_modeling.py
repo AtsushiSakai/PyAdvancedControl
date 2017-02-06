b7be89473634107f53991e486ee7174f84f8ef62
AtsushiSakai <asakai.amsl+github@gmail.com>
2017-02-05 23:02:51 -0800

    add test

--- tests/test_mpc_modeling.py
+++ tests/test_mpc_modeling.py
@@ -120,6 +120,40 @@ class Test(unittest.TestCase):
             print(i, j)
             assert (i - j) <= 0.0001, "Error"
 
+    def test_3(self):
+        A = np.matrix([[0.8, 1.0], [0, 0.9]])
+        B = np.matrix([[-1.0], [2.0]])
+        (nx, nu) = B.shape
+
+        N = 10  # number of horizon
+        Q = np.eye(nx)
+        R = np.eye(nu)
+        P = np.eye(nx)
+
+        x0 = np.matrix([[1.0], [2.0]])  # init state
+
+        x, u = mpc_modeling.mpc_modeling.use_modeling_tool(A, B, N, Q, R, P, x0)
+
+        rx1 = np.array(x[0, :]).flatten()
+        rx2 = np.array(x[1, :]).flatten()
+        ru = np.array(u[0, :]).flatten()
+
+        x, u = mpc_modeling.mpc_modeling.hand_modeling2(A, B, N, Q, R, P, x0, None, None)
+
+        x1 = np.array(x[0, :]).flatten()
+        x2 = np.array(x[1, :]).flatten()
+        u = np.array(u).flatten()
+
+        for (i, j) in zip(rx1, x1):
+            print(i, j)
+            assert (i - j) <= 0.0001, "Error"
+        for (i, j) in zip(rx2, x2):
+            print(i, j)
+            assert (i - j) <= 0.0001, "Error"
+        for (i, j) in zip(ru, u):
+            print(i, j)
+            assert (i - j) <= 0.0001, "Error"
+
 
 if __name__ == '__main__':
     unittest.main()
