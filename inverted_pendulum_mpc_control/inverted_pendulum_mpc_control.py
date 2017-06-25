09fba403f798d8e091c243466663e0a293d57138
AtsushiSakai <asakai.amsl+github@gmail.com>
2017-06-24 23:07:57 -0700

    add code

--- inverted_pendulum_mpc_control/inverted_pendulum_mpc_control.py
+++ inverted_pendulum_mpc_control/inverted_pendulum_mpc_control.py
@@ -10,8 +10,117 @@ author: Atsushi Sakai
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import time
+import cvxpy
+
 
 l_bar = 2.0  # length of bar
+M = 1.0  # [kg]
+m = 0.3  # [kg]
+g = 9.8  # [m/s^2]
+
+Q = np.diag([0.0, 1.0, 1.0, 0.0])
+R = np.diag([0.01])
+nx = 4   # 状態の数
+nu = 1   # 制御入力の数
+T = 30  # 何ステップ先まで予測するかを決める
+delta_t = 0.1
+
+
+def main():
+    x0 = np.array([
+        [0.0],
+        [0.0],
+        [0.1],
+        [0.0]
+    ])
+
+    x = np.copy(x0)
+
+    for i in range(500):
+
+        ox, dx, otheta, dtheta, ou = mpc_control(x)
+        u = ou[0]
+        x = simulation(x, u)
+        print(x)
+        print(u)
+
+        plt.clf()
+        px = float(x[0])
+        theta = float(x[2])
+        show_cart(px, theta)
+        plt.pause(0.001)
+        #  plt.show()
+
+
+def simulation(x, u):
+
+    A, B = get_model_matrix()
+
+    x = np.dot(A, x) + np.dot(B, u)
+
+    return x
+
+
+def mpc_control(x0):
+
+    x = cvxpy.Variable(nx, T + 1)
+    u = cvxpy.Variable(nu, T)
+
+    A, B = get_model_matrix()
+
+    cost = 0.0
+    constr = []
+    for t in range(T):
+        cost += cvxpy.quad_form(x[:, t + 1], Q)
+        cost += cvxpy.quad_form(u[:, t], R)
+        constr += [x[:, t + 1] == A * x[:, t] + B * u[:, t]]
+    constr += [x[:, 0] == x0]
+    prob = cvxpy.Problem(cvxpy.Minimize(cost), constr)
+
+    start = time.time()
+    prob.solve(verbose=False)
+    elapsed_time = time.time() - start
+    print("calc time:{0} [sec]".format(elapsed_time))
+
+    if prob.status == cvxpy.OPTIMAL:
+        ox = get_nparray_from_matrix(x.value[0, :])
+        dx = get_nparray_from_matrix(x.value[1, :])
+        theta = get_nparray_from_matrix(x.value[2, :])
+        dtheta = get_nparray_from_matrix(x.value[3, :])
+
+        ou = get_nparray_from_matrix(u.value[0, :])
+
+    return ox, dx, theta, dtheta, ou
+
+
+def get_nparray_from_matrix(x):
+    u"""
+    get build-in list from matrix
+    """
+    return np.array(x).flatten()
+
+
+def get_model_matrix():
+
+    # Model Parameter
+    A = np.array([
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, m * g / M, 0.0],
+        [0.0, 0.0, 0.0, 1.0],
+        [0.0, 0.0, g * (M + m) / (l_bar * M), 0.0]
+    ])
+    A = np.eye(nx) + delta_t * A
+
+    B = np.array([
+        [0.0],
+        [1.0 / M],
+        [0.0],
+        [1.0 / (l_bar * M)]
+    ])
+    B = delta_t * B
+
+    return A, B
 
 
 def flatten(a):
@@ -30,9 +139,9 @@ def show_cart(xt, theta):
 
     cx = cx + xt
 
-    bx = np.matrix([0.0, l_bar * math.sin(theta)])
+    bx = np.matrix([0.0, l_bar * math.sin(-theta)])
     bx += xt
-    by = np.matrix([cart_h, l_bar * math.cos(theta) + cart_h])
+    by = np.matrix([cart_h, l_bar * math.cos(-theta) + cart_h])
     by += radius * 2.0
 
     angles = np.arange(0.0, math.pi * 2.0, math.radians(3.0))
@@ -58,10 +167,6 @@ def show_cart(xt, theta):
     plt.axis("equal")
 
 
-def main():
-    pass
-
-
 def visualize_test():
 
     #  x = 1.0
@@ -80,5 +185,5 @@ def visualize_test():
 
 
 if __name__ == '__main__':
-    #  main()
-    visualize_test()
+    main()
+    #  visualize_test()
